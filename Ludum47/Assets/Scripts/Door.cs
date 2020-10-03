4e20568efe1e09c951c0c446c282156000d50992
Alberto Salieto <sithis77@gmail.com>
2020-10-03 19:33:40 +0200

    - Gamemanager, switch changes portal, level design stuff

--- Ludum47/Assets/Scripts/Door.cs
+++ Ludum47/Assets/Scripts/Door.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Enemy[] Enemies;
+    public OverlapDetector[] Switches;
     public GameObject[] RoomSpecificRoot; // Default could be option 0
     public DoorCombination[] Combinations;
     public Transform StartPositionTransform;
 
     private int m_currentRoom = 0;
     private List<int> m_enemiesDieOrdered = new List<int>();
-
 
     private void Start()
     {
@@ -20,7 +20,6 @@ public class Door : MonoBehaviour
             Enemies[i].OnDie = OnDieEnemy;
         }
     }
-
 
     private void OnDieEnemy(int id)
     {
@@ -31,60 +30,119 @@ public class Door : MonoBehaviour
     {
         RoomSpecificRoot[m_currentRoom].SetActive(false);
         m_currentRoom = CheckNextRoom();
-        RoomSpecificRoot[m_currentRoom].SetActive(true);
 
-        for(int i = 0;  i < Enemies.Length; ++i)
+        Debug.Log("TELEPORT TO ROOM: " + m_currentRoom);
+        if(m_currentRoom == -1)
         {
-            Enemies[i].ResetEnemy();
+            //Level win
+            GameManager.Instance.LoadNextLevel();
         }
+        else
+        {
+            RoomSpecificRoot[m_currentRoom].SetActive(true);
 
-        m_enemiesDieOrdered.Clear();
+            for (int i = 0; i < Enemies.Length; ++i)
+            {
+                Enemies[i].ResetEnemy();
+            }
+
+            m_enemiesDieOrdered.Clear();
+        }
     }
     
     private int CheckNextRoom()
     {
         for(int i = 0; i < Combinations.Length; ++i)
         {
-            for(int combiIter = 0; combiIter < Combinations[i].CodeOpen.Count; ++combiIter)
+            //FOR CHECKING ENEMIES
+            if (Combinations[i].CodeOpen.Count > 0)
             {
-                if(Combinations[i].CodeOpen.Count > m_enemiesDieOrdered.Count)
+                for (int combiIter = 0; combiIter < Combinations[i].CodeOpen.Count; ++combiIter)
                 {
-                    break;
-                }
-
-                if(Combinations[i].NeedOrder && 
-                    Combinations[i].CodeOpen[combiIter] != m_enemiesDieOrdered[combiIter])
-                {
-                    break;
-                }
-                else if (!m_enemiesDieOrdered.Contains(Combinations[i].CodeOpen[combiIter]))
-                {
-                    break;
-                }
-
-                if(combiIter == Combinations[i].CodeOpen.Count - 1)
-                {
-                    if(Combinations[i].NeedOtherAlive)
+                    if (Combinations[i].CodeOpen.Count > m_enemiesDieOrdered.Count)
                     {
-                        bool combiValid = true;
-                        for (int enemyIter = 0; enemyIter < Enemies.Length; ++enemyIter)
+                        break;
+                    }
+
+                    if (Combinations[i].NeedOrder &&
+                        Combinations[i].CodeOpen[combiIter] != m_enemiesDieOrdered[combiIter])
+                    {
+                        break;
+                    }
+                    else if (!m_enemiesDieOrdered.Contains(Combinations[i].CodeOpen[combiIter]))
+                    {
+                        break;
+                    }
+
+                    if (combiIter == Combinations[i].CodeOpen.Count - 1)
+                    {
+                        if (Combinations[i].NeedOtherAlive)
                         {
-                            if(!Combinations[i].CodeOpen.Contains(Enemies[enemyIter].EnemyID) &&
-                                !Enemies[enemyIter].IsAlive())
+                            bool combiValid = true;
+                            for (int enemyIter = 0; enemyIter < Enemies.Length; ++enemyIter)
                             {
-                                combiValid = false;
+                                if (!Combinations[i].CodeOpen.Contains(Enemies[enemyIter].EnemyID) &&
+                                    !Enemies[enemyIter].IsAlive())
+                                {
+                                    combiValid = false;
+                                    break;
+                                }
+                            }
+
+                            if (!combiValid)
+                            {
                                 break;
                             }
                         }
 
-                        if(!combiValid)
+                        if (Combinations[i].FinalComb)
                         {
+                            return -1;
+                        }
+                        else
+                        {
+                            return Combinations[i].DoorOpen;
+                        }
+                    }
+                }
+            }
+
+            //FOR CHECKING SWITCHES
+            if (Combinations[i].CodeSwitchOpen.Count > 0)
+            {
+                for (int combiIter = 0; combiIter < Combinations[i].CodeSwitchOpen.Count; ++combiIter)
+                {
+                    bool combiValid = true;
+                    for (int j = 0; j < Switches.Length; j++)
+                    {
+                        if (Switches[j].IsActive() && Switches[j].id == Combinations[i].CodeSwitchOpen[combiIter])
+                        {
+                            combiValid = true;
+                        }
+                        else
+                        {
+                            combiValid = false;
                             break;
                         }
                     }
 
-                    return Combinations[i].DoorOpen;
+                    if (combiValid)
+                    {
+                        if (Combinations[i].FinalComb)
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            return Combinations[i].DoorOpen;
+                        }
+                    }
                 }
+            }
+
+            if(Combinations[i].CodeOpen.Count == 0 && Combinations[i].CodeSwitchOpen.Count == 0)
+            {
+                return -1;
             }
         }
 
---
+++ Ludum47/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int m_currentLevel = 0;
+    /*
+    private void Start()
+    {
+        //lol this is terrible, TODO use a loader instead
+        var a = Instance;
+    }
+    */
+    public void LoadNextLevel()
+    {
+        m_currentLevel++;
+        SceneManager.LoadScene("Level" + m_currentLevel.ToString(), LoadSceneMode.Single);
+        //Application.LoadLevel("Level" + m_currentLevel.ToString());
+    }
+}
