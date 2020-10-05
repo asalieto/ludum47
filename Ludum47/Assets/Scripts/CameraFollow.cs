3bd27ed29c62631ab445ff086c4ae107dfeda0d1
Alberto Salieto <sithis77@gmail.com>
2020-10-05 20:22:54 +0200

    - Camera follow (disabled by default)

--- Ludum47/Assets/Scripts/CameraFollow.cs
+++ Ludum47/Assets/Scripts/CameraFollow.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-
-    GameObject player;
     public float speed = 5;
+    public bool teleportAtStart = false;
+
+    private GameObject player;
 
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+
+        if (teleportAtStart)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        }
     }
 
     void FixedUpdate()
     {
-        Vector2 auxPos = new Vector2(0,0);
-        auxPos.x = Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime*speed);
-        auxPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y, Time.deltaTime*speed);
+        Vector2 auxPos = new Vector2(0, 0);
+        auxPos.x = Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime * speed);
+        auxPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y, Time.deltaTime * speed);
         transform.position = new Vector3(auxPos.x, auxPos.y, transform.position.z);
     }
 }
